e03564ae72a9c1b3e174436cc4be04eb0441d4db
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-08-04 01:27:23 -0300

    basic structure

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import cors from 'cors';
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from '../src/routes/AuthRoutes.js'
+import urlsRoutes from '../src/routes/urlsRoutes.js'
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(authRoutes);
+app.use(urlsRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ` + PORT);
+});
