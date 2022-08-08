31de058efcfc249ab899ee761f93e22667d38f2f
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-08-08 12:12:41 -0300

    Returning user without shortLinks

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -16,30 +16,33 @@ export async function getUserController(req, res) {
     );
 
     if (userResponse.length === 0) {
-      const user = await connection.query(
-        `SELECT users.id AS id, users.name AS name
+      const { rows: user } = await connection.query(
+        `SELECT id, name
         FROM users
         WHERE id=$1`,
         [userId]
       );
+      response = {
+        id: userId,
+        name: user[0].name,
+        visitCount: 0,
+        shortenedUrls: [],
+      };
+    } else {
+      const { rows: urlsResponse } = await connection.query(
+        `SELECT id, "shortUrl", url, "visitCount"
+          FROM urls
+          WHERE "userId" = $1`,
+        [userId]
+      );
 
-      return res.status(200).send(user);
+      response = {
+        id: userId,
+        name: userResponse[0].name,
+        visitCount: userResponse[0].visitCount,
+        shortenedUrls: urlsResponse,
+      };
     }
-
-    const { rows: urlsResponse } = await connection.query(
-      `SELECT id, "shortUrl", url, "visitCount"
-        FROM urls
-        WHERE "userId" = $1`,
-      [userId]
-    );
-    
-    response = {
-      id: userId,
-      name: userResponse[0].name,
-      visitCount: userResponse[0].visitCount,
-      shortenedUrls: urlsResponse,
-    };
-
     return res.status(200).send(response);
   } catch (e) {
     return res.status(500).send(e.message);
