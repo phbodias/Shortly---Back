d3a07dd8912bf80b7dad1ccc60edcd933ff852bc
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-08-08 11:34:28 -0300

    Getting ranking

--- src/controllers/rankingController.js
+++ src/controllers/rankingController.js
@@ -8,7 +8,7 @@ export async function getRanking(req, res) {
     JOIN urls
     ON users.id=urls."userId"
     GROUP BY users.id
-    ORDER BY count(urls."visitCount") desc
+    ORDER BY "visitsCount" desc
     LIMIT 10
     `);
 
